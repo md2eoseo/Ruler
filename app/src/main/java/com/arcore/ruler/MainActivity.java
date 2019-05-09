8d75c35a8d259764e6c883140b8602e606c4aa1d
jkpc5959 <jkpc5959@naver.com>
2019-05-09 16:15:40 +0900

    MeasureActivity.java glsurfaceview - draw camera image on screen declare glsurfaceview object

--- app/src/main/java/com/arcore/ruler/MainActivity.java
+++ app/src/main/java/com/arcore/ruler/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.google.ar.core.Session;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_measure; //ㅁㄴㅇㄻㄴㅇㄹ
+
+    Button btn_measure;
     Button btn_locate;
     Button btn_exit;
 
@@ -27,21 +29,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MeasureActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        btn_locate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MeasureActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
             }
         });
     }
--- app/src/main/java/com/arcore/ruler/MeasureActivity.java
+++ app/src/main/java/com/arcore/ruler/MeasureActivity.java
@@ -1,13 +1,23 @@
 package com.arcore.ruler;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import com.google.ar.core.Session;
+
 public class MeasureActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private GLSurfaceView mSurfaceView;
+    //private MainRednerer mRenderer;
+    Session mSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measure);
+
+        mSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
     }
 }
