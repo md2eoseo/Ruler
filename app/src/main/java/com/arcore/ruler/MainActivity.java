b567cb8b55771f652655924ea3e10ad17665c62e
luckynwj7 <luckynwj7@naver.com>
2019-05-09 14:10:44 +0900

    first11

--- app/src/main/java/com/arcore/ruler/MainActivity.java
+++ app/src/main/java/com/arcore/ruler/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"ㅁㄴㅇㄹ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"ㅁㄴㅇㄹd", Toast.LENGTH_SHORT).show();
     }
 }
