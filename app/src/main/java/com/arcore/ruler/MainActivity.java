7fa5f258f5ef0c81cd0690b7fab3dee192cfd798
luckynwj7 <luckynwj7@naver.com>
2019-05-09 15:33:13 +0900

    액티비티 이동 구현 (main -> measure)

--- app/src/main/java/com/arcore/ruler/MainActivity.java
+++ app/src/main/java/com/arcore/ruler/MainActivity.java
@@ -1,15 +1,33 @@
 package com.arcore.ruler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_measure;
+    Button btn_locate;
+    Button btn_exit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"ㅁㄴㅇㄹd", Toast.LENGTH_SHORT).show();
+
+        btn_measure = (Button)findViewById(R.id.btn_measure);
+        btn_locate = (Button)findViewById(R.id.btn_locate);
+        btn_exit = (Button)findViewById(R.id.btn_exit);
+
+        btn_measure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MeasureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/arcore/ruler/MeasureActivity.java
@@ -0,0 +1,13 @@
+package com.arcore.ruler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeasureActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measure);
+    }
+}
