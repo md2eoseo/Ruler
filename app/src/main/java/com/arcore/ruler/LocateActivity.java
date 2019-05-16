c41b2ca9765795b333b24fdb49e268d2a4ac8aae
md2eoseo <md2eoseo@gmail.com>
2019-05-16 14:44:08 +0900

    LocateActivity.java 작성

--- app/src/main/java/com/arcore/ruler/LocateActivity.java
+++ app/src/main/java/com/arcore/ruler/LocateActivity.java
@@ -1,13 +1,151 @@
 package com.arcore.ruler;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.PointCloud;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+
+import java.util.List;
 
 public class LocateActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private boolean mUserRequestedInstall = true;
+
+    private TextView mTextView;
+    private GLSurfaceView mSurfaceView;
+
+    private MainRenderer mRenderer;
+
+    private Session mSession;
+    private Config mConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideStatusBarAndTitleBar();
         setContentView(R.layout.activity_locate);
+
+        mTextView = (TextView) findViewById(R.id.txt_locate);
+        mSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
+
+        mRenderer = new MainRenderer(new MainRenderer.RenderCallback(){
+            @Override
+            public void preRender() {
+                if (mRenderer.isViewportChanged()) {
+                    Display display = getWindowManager().getDefaultDisplay();
+                    int displayRotation = display.getRotation();
+                    mRenderer.updateSession(mSession, displayRotation);
+                }
+
+                mSession.setCameraTextureName(mRenderer.getTextureId());
+
+                Frame frame = mSession.update();
+                if (frame.hasDisplayGeometryChanged()) {
+                    mRenderer.transformDisplayGeometry(frame);
+                }
+
+                PointCloud pointCloud = frame.acquirePointCloud();
+                mRenderer.updatePointCloud(pointCloud);
+                pointCloud.release();
+
+//                if (mPointAdded) {
+//                    List<HitResult> results = frame.hitTest(mLastX, mLastY);
+//                    for (HitResult result : results) {
+//                        Pose pose = result.getHitPose();
+//                        float[] points = new float[]{ pose.tx(), pose.ty(), pose.tz() };
+//                        mPoints.add(points);
+//                        mRenderer.addPoint(points);
+//                        updateDistance();
+//                    }
+//                    mPointAdded = false;
+//                }
+
+                Camera camera = frame.getCamera();
+                float[] projMatrix = new float[16];
+                camera.getProjectionMatrix(projMatrix, 0, 0.1f, 100.0f);
+                float[] viewMatrix = new float[16];
+                camera.getViewMatrix(viewMatrix, 0);
+
+                mRenderer.setProjectionMatrix(projMatrix);
+                mRenderer.updateViewMatrix(viewMatrix);
+            }
+        });
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setRenderer(mRenderer);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        requestCameraPermission();
+
+        try {
+            if (mSession == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        mSession = new Session(this);
+                        Log.d(TAG, "ARCore Session created.");
+                        break;
+                    case INSTALL_REQUESTED:
+                        mUserRequestedInstall = false;
+                        Log.d(TAG, "ARCore should be installed.");
+                        break;
+                }
+            }
+        }
+        catch (UnsupportedOperationException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        mConfig = new Config(mSession);
+        if (!mSession.isSupported(mConfig)) {
+            Log.d(TAG, "This device is not support ARCore.");
+        }
+        mSession.configure(mConfig);
+        mSession.resume();
+
+        mSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mSurfaceView.onPause();
+        mSession.pause();
+    }
+
+    private void requestCameraPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, 0);
+        }
+    }
+
+    private void hideStatusBarAndTitleBar(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
